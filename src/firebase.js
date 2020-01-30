ffdd01907dc613365b4d33aae72950c477ac3857
M0ng00se7169 <NickKomarowski@gmail.com>
2020-01-30 10:40:41 +0100

    Config vars

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,14 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import * as keys from './config/keys'
 
-const firebaseConfig = firebase.initializeApp({keys});
+const firebaseConfig = firebase.initializeApp({
+  apiKey: keys.apiKey,
+  authDomain: keys.authDomain,
+  databaseURL: keys.databaseURL,
+  projectId: keys.projectId,
+  storageBucket: keys.storageBucket,
+  messagingSenderId: keys.messagingSenderId,
+  appId: keys.appId  
+});
 
 export { firebaseConfig as firebase };
